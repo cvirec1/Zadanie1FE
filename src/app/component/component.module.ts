75b7ac0ca3580a3ead5b68ffdbad401747f030c0
valjasek <valjasek@kros.sk>
2021-01-19 09:24:54 +0100

    úpravy

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../home/home.component';
-import { NavBarComponent } from './nav-bar-modul/nav-bar/nav-bar.component';
+import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 import { WorkItemComponent } from './work-content/work-detail-modul/workItem/work-item.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { WorkItemModule } from './work-content/work-item.module';
-import { SearchInputComponent } from './nav-bar-modul/search-input/search-input.component';
+import { SearchInputComponent } from '../shared/search-input/search-input.component';
 
 const COMPONENTS = [HomeComponent, NavBarComponent, WorkItemComponent, SearchInputComponent];
 
